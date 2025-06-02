75d8d8d618ac1216c22ab888b6fa4fa0961adeab
MonkeyBank <t.one.h.long@gmail.com>
2025-06-02 17:21:27 +0900

    done

---
+++ build/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import View from './View';
+import { ToastContainer } from 'react-toastify';
+
+const Home = () => {
+  return (
+    <div>
+      <View />
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Home;
